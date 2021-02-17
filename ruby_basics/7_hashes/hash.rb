ff719afdebc04e714f02c8cd281d512165efa7c6
Michael Antis <antis.michael@gmail.com>
2021-02-17 11:58:03 -0500

    complete hash exercises

--- ruby_basics/7_hashes/hash.rb
+++
@@ -1,5 +0,0 @@
-original_list = { color: 'blue', number: 65, shape: 'circle', movie: 'Avengers: Endgame', animal: 'cat'}
-            
-additional_list = { color: 'red', shape: 'cyclinder'}
-
-p original_list.merge!(additional_list)
