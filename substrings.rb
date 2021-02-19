b9e03fded2b8001d2a29b8bf2842ebb684d74d1c
Michael Antis <antis.michael@gmail.com>
2021-02-18 20:56:51 -0500

    complete substrings exercise

--- substrings.rb
+++ substrings.rb
@@ -1,27 +1,9 @@
-# Implement a method #substrings that takes a string as the first argument and then 
-# an array of valid substrings (your dictionary) as the second argument. It should 
-# return a hash listing each substring (case insensitive) that was found in the 
-# original string and how many times it was found.
-
-
 def substrings(string, dictionary)
-  matchers = {}
-
-    string.split(' ').map do |input_word|
-      dictionary.each do |dictionary_word| 
-        matchers[dictionary_word] = input_word if input_word.downcase.include? dictionary_word
-      end
-
-
-
-      # matchers[input_word] = string.count(input_word)
-    end
-puts matchers
+  substrings_found = {}
+  string = string.gsub(/[^0-9a-z ]/i, '').downcase
+  dictionary.each { |word| substrings_found[word] = string.scan("#{word}").count unless !string.include? word }
+  substrings_found
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("Howdy partner, sit down! How's it going?", dictionary) # ---> { "below" => 1, "low" => 1 }
-
-
-# => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3,
-# =>   "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
